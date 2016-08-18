9a4b79e9aabfcb74e8f970a58eeb2dd26173114c
Thiago Mello <thiagomellox@hotmail.com>
2016-08-18 10:58:41 -0300

    ignore

---
+++ bin/src/main/webapp/js/app.js
@@ -0,0 +1,32 @@
+var $stateProviderRef = null;
+
+angular.module("webApp", ["ui.router", "hs.WebService", 'ui.mask','ui.bootstrap'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+    	// For any unmatched url, send to /index
+		  $urlRouterProvider.otherwise("/listartista");
+
+		  $stateProvider
+		  .state('list_artista', {
+		      url: "/listartista",
+		      templateUrl: "pages/artista/list.html",
+		      controller: "ArtistaListController"
+		    })
+		    .state('incluir_artista', {
+		      url: "/incluirartista",
+		      templateUrl: "pages/artista/incluir.html",
+		      controller: "ArtistaIncluirController"
+		    })
+		  	
+		  
+		  .state('list_musica', {
+			  url: "/listmusica",
+			  templateUrl: "pages/musica/list.html",
+			  controller: "MusicaListController"
+		  })
+		  .state('incluir_musica', {
+			  url: "/incluirmusica",
+			  templateUrl: "pages/musica/incluir.html",
+			  controller: "MusicaIncluirController"
+		  });
+    	
+    });
